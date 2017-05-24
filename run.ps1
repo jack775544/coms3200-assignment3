6ac7b74b0a1de93790a09ca1efc45b06cb118261
Jack Kerr <jack775544@gmail.com>
2017-05-24 11:05:04 +1000

    Updated run script and added comments

--- run.ps1
+++ run.ps1
@@ -1,14 +1,33 @@
+Write-Host -ForegroundColor Yellow "HTTP File. Will Succeed"
 Write-Host "python .\webget.py http://www.rfc-base.org/txt/rfc-768.txt"
 & python .\webget.py http://www.rfc-base.org/txt/rfc-768.txt
+Write-Host ""
+
+Write-Host -ForegroundColor Yellow "FTP File. Will Succeed"
 Write-Host "python .\webget.py ftp://ftp.uq.edu.au/welcome.msg"
 & python .\webget.py ftp://ftp.uq.edu.au/welcome.msg
-Write-Host ".\webget.py ftp://ftp.marine.csiro.au/pub/spencer_gulf/met/README"
+Write-Host ""
+
+Write-Host -ForegroundColor Yellow "FTP File. Will succeed"
+Write-Host "python .\webget.py ftp://ftp.marine.csiro.au/pub/spencer_gulf/met/README"
 & python .\webget.py ftp://ftp.marine.csiro.au/pub/spencer_gulf/met/README
+Write-Host ""
+
+Write-Host -ForegroundColor Yellow "FTP File. File not found"
 Write-Host "python .\webget.py ftp://ftp.uq.edu.au/welcome.txt"
 & python .\webget.py ftp://ftp.uq.edu.au/welcome.txt
+Write-Host ""
+
+Write-Host -ForegroundColor Yellow "FTP File. Wrong folder"
 Write-Host "python .\webget.py ftp://ftp.uq.edu.au/toplevel/welcome.msg"
 & python .\webget.py ftp://ftp.uq.edu.au/toplevel/welcome.msg
+Write-Host ""
+
+Write-Host -ForegroundColor Yellow "FTP File. Incorrect Hostname"
 Write-Host "python .\webget.py ftp://ftp.itee.uq.edu.au/welcome.msg"
 & python .\webget.py ftp://ftp.itee.uq.edu.au/welcome.msg
+Write-Host ""
+
+Write-Host -ForegroundColor Yellow "Unimplemented Protocol"
 Write-Host "python .\webget.py ftps://ftp.uq.edu.au/welcome.msg"
 & python .\webget.py ftps://ftp.uq.edu.au/welcome.msg
--- webget.py
+++ webget.py
@@ -9,41 +9,55 @@ from urllib.parse import urlparse
 from ftplib import FTP
 
 def downloadHttp(url):
+    # Segment up the url
     segments = urlparse(url)
     filename = 'index' if segments.path == '' or segments.path == '/' else segments.path.split('/')[-1]
     site = None
     try:
+        # Attempt to download the site
         site = urllib.request.urlopen(url)
     except urllib.error.HTTPError as g:
+        # Invalid file (404), throw error
         print(g)
         return
     except urllib.error.URLError as e:
+        # Invalid hostname, throw error
         print('URL Error: Hostname Not Found')
         return
     
+    # Save the file
     with open(filename, 'wb') as f:
         f.write(site.read())
 
 def downloadFtp(url):
+    # Segment the url
     segments = urlparse(url)
     path_segments = segments.path.strip('/').rsplit('/', 1)
     filename = path_segments[-1]
     folder = path_segments[0] if path_segments[0] != path_segments[-1] else None
+
     try:
+        # Set up ftp connection
         ftp = FTP(segments.netloc)
     except:
+        # If this fails the hostname is wrong
         print('Incorrect hostname')
         return
+    # Login as anon
     ftp.login('anonymous','coms3200@uq.edu.au')
     if folder is not None:
         try:
+            # Change to the specified folder
             ftp.cwd(folder)
         except:
+            # Error if the folder does not exist
             print("The folder does not exist on the FTP server")
             return
     try:
+        # Download and save the file
         ftp.retrbinary(f'RETR {filename}', open(filename, 'wb').write)
     except:
+        # Error if the file is not there
         print("The file could not be found on the FTP server")
 
 
@@ -52,11 +66,13 @@ def main():
     assert len(argv) == 2
     url = argv[1]
 
+    # Match the protocol
     if re.match("http:\/\/(.*)", url):
         downloadHttp(url)
     elif re.match("ftp:\/\/(.*)", url):
         downloadFtp(url)
     else:
+        # Error if the wrong protocol is given
         print("Invalid Protocol")
 
 if __name__ == "__main__":
